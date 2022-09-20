cdfd73e9668de843cac9f7e40983753bfd86ca3a
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-20 18:25:43 +0300

    Seminars_Task1

--- Seminars/Task0/Program.cs
+++ Seminars/Task0/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Введите значение");
+﻿/*0. Напишите программу, которая на вход принимает число и выдаёт его квадрат 
+(число умноженное на само себя).
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите значение");
 int userNumber = Convert.ToInt32(Console.ReadLine());
 int result = userNumber * userNumber;
 Console.WriteLine($"Квадрат числа {userNumber} равен {result}");
---
+++ Seminars/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*1. Напишите программу, которая на вход принимает два числа и проверяет,
+ является ли первое число квадратом второго.
+a = 25, b = 5 -> да
+a = 2, b = 10 -> нет
+a = 9, b = -3 -> да
+a = -3 b = 9 -> нет
+*/
+Console.Write("Введите число A: ");
+int userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int userNumberB = Convert.ToInt32(Console.ReadLine());
+int square = userNumberB * userNumberB;
+if (square == userNumberA) 
+{
+    Console.WriteLine("Да!");
+}
+else 
+{
+    Console.WriteLine("Нет!");
+}

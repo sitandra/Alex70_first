b3128db6ee02f1269486faa1fbb95760295cf18a
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-20 19:31:29 +0300

    Seminars_Task3_1

--- Seminars/Task3/Program.cs
+++ Seminars/Task3/Program.cs
@@ -3,10 +3,31 @@
 5 -> Пятница
 */
 int number = new Random().Next(1,8);
-if (number == 1) Console.WriteLine ("1 - Понедельник!");
-if (number == 2) Console.WriteLine ("2 - Вторник!");
-if (number == 3) Console.WriteLine ("3 - Среда!");
-if (number == 4) Console.WriteLine ("4 - Четверг!");
-if (number == 5) Console.WriteLine ("5 - Пятница!");
-if (number == 6) Console.WriteLine ("6 - Суббота!");
-if (number == 7) Console.WriteLine ("7 - Воскресенье!");
+if (number == 1) 
+{
+    Console.WriteLine ("1 - Понедельник!");
+}
+else if (number == 2) 
+{
+    Console.WriteLine ("2 - Вторник!");
+}
+else if (number == 3) 
+{
+    Console.WriteLine ("3 - Среда!");
+}
+else if (number == 4) 
+{
+    Console.WriteLine ("4 - Четверг!");
+}
+else if (number == 5) 
+{
+    Console.WriteLine ("5 - Пятница!");
+}
+else if (number == 6) 
+{
+    Console.WriteLine ("6 - Суббота!");
+}
+else if (number == 7) 
+{
+    Console.WriteLine ("7 - Воскресенье!");
+}

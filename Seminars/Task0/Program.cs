5d7aa22cb13479a0c29145654b7df5c252bb1b8f
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-20 16:12:14 +0300

    Seminars_Task0

---
+++ Seminars/Task0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите значение");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = userNumber * userNumber;
+Console.WriteLine($"Квадрат числа {userNumber} равен {result}");

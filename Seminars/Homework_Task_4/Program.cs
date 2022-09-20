e719fe0df299bcf2c91ce296cf425eb4c0ddd095
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-20 23:34:11 +0300

    Homework_Task_4

---
+++ Seminars/Homework_Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа
+ и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int numberA = new Random().Next(1, 100);
+int numberB = new Random().Next(1, 100);
+int numberC = new Random().Next(1, 100);
+Console.WriteLine($"Введите три числа: {numberA}, {numberB}, {numberC}");
+int max = numberA;
+if (numberB > numberA)
+{
+    max = numberB;
+    if (numberC > numberB)
+    {
+        max = numberC;
+    }
+    else if (numberC > numberA)
+    {
+        max = numberC;
+    }
+    else 
+    max = numberB;
+}
+Console.WriteLine($"Число max: {max}");

ee307ed8ba2f96a7c1eb91fb4c26299bbbd9bbd6
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-20 19:15:05 +0300

    Seminars_Task3

---
+++ Seminars/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница
+*/
+int number = new Random().Next(1,8);
+if (number == 1) Console.WriteLine ("1 - Понедельник!");
+if (number == 2) Console.WriteLine ("2 - Вторник!");
+if (number == 3) Console.WriteLine ("3 - Среда!");
+if (number == 4) Console.WriteLine ("4 - Четверг!");
+if (number == 5) Console.WriteLine ("5 - Пятница!");
+if (number == 6) Console.WriteLine ("6 - Суббота!");
+if (number == 7) Console.WriteLine ("7 - Воскресенье!");

be506fd1c3a96c510b6d8f9a76132c35570fe38f
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-22 15:05:18 +0300

    Sem_2_Task_2

---
+++ Seminars/Sem_2_Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число и 
+удаляет вторую цифру этого числа. 
+456 -> 46 
+782 -> 72 
+918 -> 98
+*/
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число: {number} ");
+int firstNumber = number / 100;
+int lastNumber = number % 10;
+Console.WriteLine($"Итоговое число: {firstNumber}{lastNumber} ");
